bc7d8b2a9ac510f64dbd55ddbe8ac92570d54d07
soorajks21 <soorajks@live.in>
2019-03-19 13:17:38 -0400

    Added Bootstrap anf Front-awesome

--- SpiderWeb.API/Startup.cs
+++ SpiderWeb.API/Startup.cs
@@ -30,6 +30,7 @@ namespace SpiderWeb.API
         {
             services.AddDbContext<DataContext>(x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -43,7 +44,7 @@ namespace SpiderWeb.API
             {
               //  app.UseHsts();
             }
-
+            app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
          //   app.UseHttpsRedirection();
             app.UseMvc();
         }
