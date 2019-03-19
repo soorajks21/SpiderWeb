98d29c6f867665ea9378c6554cc99badf0c6c892
soorajks21 <soorajks@live.in>
2019-03-18 23:11:31 -0400

    initial commit

---
+++ SpiderWeb.API/Data/DataContext.cs
@@ -0,0 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using SpiderWeb.API.Models;
+
+namespace SpiderWeb.API.Data
+{
+    public class DataContext : DbContext
+    {
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options){}
+
+        public DbSet<Value> Values { get; set; }
+        
+        
+        
+    }
+}
---
+++ SpiderWeb.API/Models/Value.cs
@@ -0,0 +1,14 @@
+
+namespace SpiderWeb.API.Models
+{
+    public class Value
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+        
+
+
+        
+    }
+}
