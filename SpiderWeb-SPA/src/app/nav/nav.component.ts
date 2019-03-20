bdd7b42b5149bd935e2d7ba461923362d320dd1e
soorajks21 <soorajks@live.in>
2019-03-20 18:44:27 -0400

    added UI from login and implemented service call in angular for login

---
+++ SpiderWeb-SPA/src/app/nav/nav.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../_services/auth.service';
+import { error } from '@angular/compiler/src/util';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.css']
+})
+export class NavComponent implements OnInit {
+
+ 
+  constructor(private authService: AuthService) { }
+  model: any = {}
+
+  ngOnInit() {
+  }
+
+  login(){
+  this.authService.login(this.model).subscribe(next =>{
+    console.log(' Logged ');
+  },
+   error => {
+    console.log('not logged');
+  } 
+  )}
+
+}
